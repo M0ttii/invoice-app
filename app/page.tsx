295e00d6ad8e82319a53a52c8abe925af949609f
Adrian <adrian.doernberg@berlin.de>
2024-04-10 15:38:17 +0200

    Fixed bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,39 @@
 import Pricing from '@/components/ui/Pricing/Pricing';
 import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
 export default async function PricingPage() {
-  const supabase = createClient();
+  return redirect('/login');
+  // const supabase = createClient();
 
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
+  // const {
+  //   data: { user }
+  // } = await supabase.auth.getUser();
 
-  const { data: subscription, error } = await supabase
-    .from('subscriptions')
-    .select('*, prices(*, products(*))')
-    .in('status', ['trialing', 'active'])
-    .maybeSingle();
+  // const { data: subscription, error } = await supabase
+  //   .from('subscriptions')
+  //   .select('*, prices(*, products(*))')
+  //   .in('status', ['trialing', 'active'])
+  //   .maybeSingle();
 
-  if (error) {
-    console.log(error);
-  }
+  // if (error) {
+  //   console.log(error);
+  // }
 
-  const { data: products } = await supabase
-    .from('products')
-    .select('*, prices(*)')
-    .eq('active', true)
-    .eq('prices.active', true)
-    .order('metadata->index')
-    .order('unit_amount', { referencedTable: 'prices' });
+  // const { data: products } = await supabase
+  //   .from('products')
+  //   .select('*, prices(*)')
+  //   .eq('active', true)
+  //   .eq('prices.active', true)
+  //   .order('metadata->index')
+  //   .order('unit_amount', { referencedTable: 'prices' });
 
   return (
-    <Pricing
-      user={user}
-      products={products ?? []}
-      subscription={subscription}
-    />
+    // <Pricing
+    //   user={user}
+    //   products={products ?? []}
+    //   subscription={subscription}
+    // />
+    <div className=""></div>
   );
 }
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -10,36 +10,36 @@ type AccountPair = {
 
 export const revalidate = 60;
 
-export const getAccountPairs = cache(async (): Promise<AccountPair[]> => {
-  console.log("call");
-  return new Promise(async (resolve, reject) => {
-      try {
-          const supabase = createClient();
-          const { data: keys } = await supabase.from('stripekeys').select('*');
-
-          if (!keys) {
-              resolve([]);
-              return;
-          }
-
-          const accountPairsPromises = keys.map(key => {
-              const stripe = new Stripe(key.key || '', {
-                  apiVersion: '2023-10-16',
-              });
-
-              return stripe.accounts.retrieve()
-                  .then(accountName => ({ account: accountName, key: key.key || '' }));
-          });
-
-          const accountPairs = await Promise.all(accountPairsPromises);
-          resolve(accountPairs);
-      } catch (error) {
-          reject(error);
-      }
-  });
-});
-
 export default async function SettingsPage() {
+  const getAccountPairs = cache(async (): Promise<AccountPair[]> => {
+    console.log("call");
+    return new Promise(async (resolve, reject) => {
+        try {
+            const supabase = createClient();
+            const { data: keys } = await supabase.from('stripekeys').select('*');
+  
+            if (!keys) {
+                resolve([]);
+                return;
+            }
+  
+            const accountPairsPromises = keys.map(key => {
+                const stripe = new Stripe(key.key || '', {
+                    apiVersion: '2023-10-16',
+                });
+  
+                return stripe.accounts.retrieve()
+                    .then(accountName => ({ account: accountName, key: key.key || '' }));
+            });
+  
+            const accountPairs = await Promise.all(accountPairsPromises);
+            resolve(accountPairs);
+        } catch (error) {
+            reject(error);
+        }
+    });
+  });
+
   const data = await getAccountPairs();
 
   return (
